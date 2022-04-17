83633fe2eda35f7e09932f0e509fe22e1f1269fb
Heesang Yoon <85086571+hsyoon2261@users.noreply.github.com>
2022-04-17 18:06:12 +0900

    lock

---
+++ ReaderWriterLock/Lock.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading;
+
+//락-프리 프로그래밍
+namespace ReaderWriterLock
+{
+    // 재귀적 락을 허용할지(No)
+    // writelock acquire 한 상태에서 또다시 재귀적으로 같은 thread에서 acquire 할때 그것을 허용해 줄 것인지.
+    // 스핀락 정책(5000번->yeild)
+    public class Lock
+    {
+        private const int EMPTY_FLAG = 0x00000000;
+        //음수부분
+
+        private const int WRITE_MASK = 0x7FFF0000;
+        private const int READ_MASK = 0x0000FFFF;
+
+        private const int MAX_SPIN_COUNT = 5000;
+        //int 는 32비트로 구성되어있다. 계산기 -프로그래머 항목 참고
+
+        // [Unused(1)][WriteThreadId(15)][ReadCount(16)]
+        private int _flag = EMPTY_FLAG;
+
+        public void WriteLock()
+        {
+            while (true)
+            {
+                //아무도 writelock or readlock 을 획득하고 있지 않을 때, 경합해서 소유권을 얻는다.
+                int desired = (Thread.CurrentThread.ManagedThreadId << 16) & WRITE_MASK;
+                for (int i = 0; i < MAX_SPIN_COUNT; i++)
+                {
+                    //락프리 프로그래밍 패턴
+                    if (Interlocked.CompareExchange(ref _flag, desired, EMPTY_FLAG) == EMPTY_FLAG)
+                        return;
+
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void WriteUnlock()
+        {
+            Interlocked.Exchange(ref _flag, EMPTY_FLAG);
+        }
+
+        public void ReadLock()
+        {
+            for (int i = 0; i < MAX_SPIN_COUNT; i++)
+            {
+                int expected = (_flag & READ_MASK); // flag 에 readmask 연산 = writemask empty인지 체크하는거다 사실상
+                //readmask 가 32중에 0~15 부분 읽는것 이므로 그냥 +1 해도 되는거임. 
+                //아무도 WriteLock을 획득하고 잇지 않으면(expected와 _flag가 같다면 = _flag를 writelock이 안건드렸다면) ReadCount를 1 늘린다.
+                if (Interlocked.CompareExchange(ref _flag, expected + 1, expected) == expected)
+                {
+                    //Interlocked 로 read 행위 경합// 락프리 프로그래밍 패턴 
+                    return;
+                }
+
+                Thread.Yield();
+            }
+        }
+
+        public void ReadUnlock()
+        {
+            Interlocked.Decrement(ref _flag);
+        }
+    }
+}
---
+++ ReaderWriterLock/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReaderWriterLock
+{
+    class Program
+    {
+        private static volatile int count = 0;
+        private static volatile int count2 = 0;
+
+        private static volatile int count3 = 0;
+
+        private static Lock _lock = new Lock();
+
+        static void Main(string[] args)
+        {
+            Task t1 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t2 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count--;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t3 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count2++;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t4 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.WriteLock();
+                    count2--;
+                    _lock.WriteUnlock();
+                }
+            });
+            Task t5 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.ReadLock();
+                    count3++;
+                    _lock.ReadUnlock();
+                }
+            });
+            Task t6 = new Task(delegate()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.ReadLock();
+                    count3++;
+                    _lock.ReadUnlock();
+                }
+            });
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
+            t6.Start();
+            
+            Task.WaitAll(t1, t2,t3,t4,t5,t6);
+            Console.WriteLine(count);
+            Console.WriteLine(count2);
+            Console.WriteLine(count3);
+        }
+    }
+}
